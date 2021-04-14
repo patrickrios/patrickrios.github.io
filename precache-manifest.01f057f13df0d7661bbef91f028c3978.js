483dfea7cf1eea8f6ce4e4bdfc55584440ac7797
Patrick Rios <patrick.33.rios@hotmail.com>
2021-04-14 14:51:51 -0400

    Updates

---
+++ precache-manifest.01f057f13df0d7661bbef91f028c3978.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "019d378fa5bfa62061f4b6a1a32ff289",
+    "url": "/patrickrios.github.io/index.html"
+  },
+  {
+    "revision": "02360b9a35b3797ed49b",
+    "url": "/patrickrios.github.io/static/css/main.8c7a3df4.chunk.css"
+  },
+  {
+    "revision": "4af154899df7151cabba",
+    "url": "/patrickrios.github.io/static/js/2.a4ab6310.chunk.js"
+  },
+  {
+    "revision": "64f54ce285484345cd5e3bd96b120148",
+    "url": "/patrickrios.github.io/static/js/2.a4ab6310.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02360b9a35b3797ed49b",
+    "url": "/patrickrios.github.io/static/js/main.0f406125.chunk.js"
+  },
+  {
+    "revision": "e8f94aedd6b3e436be6f",
+    "url": "/patrickrios.github.io/static/js/runtime-main.895ec8b2.js"
+  },
+  {
+    "revision": "f3dbae4fb8657fdd6da740f3213b8372",
+    "url": "/patrickrios.github.io/static/media/Logomarca.f3dbae4f.svg"
+  },
+  {
+    "revision": "34058d99b5fa45ee44c33e8598d08a4e",
+    "url": "/patrickrios.github.io/static/media/Patrick-Fernandes-Rios-Curriculo.34058d99.pdf"
+  },
+  {
+    "revision": "0836596594b409f10be567a155de9811",
+    "url": "/patrickrios.github.io/static/media/about-desc-logo.08365965.png"
+  },
+  {
+    "revision": "a33e03acd93d17cd546ef525fe16f114",
+    "url": "/patrickrios.github.io/static/media/backend-icon.a33e03ac.svg"
+  },
+  {
+    "revision": "df2651b08f8ab275ef8a0516e51b932d",
+    "url": "/patrickrios.github.io/static/media/frontend-icon.df2651b0.svg"
+  },
+  {
+    "revision": "26519ea38b8341db404172cf84c17fca",
+    "url": "/patrickrios.github.io/static/media/header-bg.26519ea3.png"
+  },
+  {
+    "revision": "6ca73cd3092fbb93977c4926462e4f15",
+    "url": "/patrickrios.github.io/static/media/header-chevron.6ca73cd3.svg"
+  },
+  {
+    "revision": "cdb683e24f0c2b8199a7d500f850a47e",
+    "url": "/patrickrios.github.io/static/media/header-profile-picture.cdb683e2.png"
+  },
+  {
+    "revision": "e404463ab12d134ec5852f637ea8019a",
+    "url": "/patrickrios.github.io/static/media/logo-map-marker.e404463a.svg"
+  },
+  {
+    "revision": "3e4f930922af149af3d5935cce742d16",
+    "url": "/patrickrios.github.io/static/media/logo-stack.3e4f9309.svg"
+  },
+  {
+    "revision": "c0c2eafc61e6b64b96e7f9a1686b9f69",
+    "url": "/patrickrios.github.io/static/media/mobile-header-bg.c0c2eafc.png"
+  }
+]);
