0f9d24e513afbb3e180102f50137b6110b8bb839
Patrick Rios <patrick.33.rios@hotmail.com>
2021-05-27 10:04:12 -0400

    Updates

---
+++ precache-manifest.4d5b02de23377ba7afe9b89fde87b5fc.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4a3bcc18c029a14cd9b7ec3bccf711b",
+    "url": "/patrickrios.github.io/index.html"
+  },
+  {
+    "revision": "23290c0caa4090521cf3",
+    "url": "/patrickrios.github.io/static/css/main.27ac24c2.chunk.css"
+  },
+  {
+    "revision": "ab56f271d7c15c39b01b",
+    "url": "/patrickrios.github.io/static/js/2.e33d27de.chunk.js"
+  },
+  {
+    "revision": "64f54ce285484345cd5e3bd96b120148",
+    "url": "/patrickrios.github.io/static/js/2.e33d27de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23290c0caa4090521cf3",
+    "url": "/patrickrios.github.io/static/js/main.74e9de2b.chunk.js"
+  },
+  {
+    "revision": "7babfd5d90bc3291fac8",
+    "url": "/patrickrios.github.io/static/js/runtime-main.a1251fb8.js"
+  },
+  {
+    "revision": "f3dbae4fb8657fdd6da740f3213b8372",
+    "url": "/patrickrios.github.io/static/media/Logomarca.f3dbae4f.svg"
+  },
+  {
+    "revision": "34058d99b5fa45ee44c33e8598d08a4e",
+    "url": "/patrickrios.github.io/static/media/Patrick-Fernandes-Rios-Curriculo.34058d99.pdf"
+  },
+  {
+    "revision": "0836596594b409f10be567a155de9811",
+    "url": "/patrickrios.github.io/static/media/about-desc-logo.08365965.png"
+  },
+  {
+    "revision": "a33e03acd93d17cd546ef525fe16f114",
+    "url": "/patrickrios.github.io/static/media/backend-icon.a33e03ac.svg"
+  },
+  {
+    "revision": "df2651b08f8ab275ef8a0516e51b932d",
+    "url": "/patrickrios.github.io/static/media/frontend-icon.df2651b0.svg"
+  },
+  {
+    "revision": "2717ce256f5af0869ad20804ad5eccad",
+    "url": "/patrickrios.github.io/static/media/glitch-logo.2717ce25.webm"
+  },
+  {
+    "revision": "26519ea38b8341db404172cf84c17fca",
+    "url": "/patrickrios.github.io/static/media/header-bg.26519ea3.png"
+  },
+  {
+    "revision": "6ca73cd3092fbb93977c4926462e4f15",
+    "url": "/patrickrios.github.io/static/media/header-chevron.6ca73cd3.svg"
+  },
+  {
+    "revision": "e404463ab12d134ec5852f637ea8019a",
+    "url": "/patrickrios.github.io/static/media/logo-map-marker.e404463a.svg"
+  },
+  {
+    "revision": "3e4f930922af149af3d5935cce742d16",
+    "url": "/patrickrios.github.io/static/media/logo-stack.3e4f9309.svg"
+  },
+  {
+    "revision": "c0c2eafc61e6b64b96e7f9a1686b9f69",
+    "url": "/patrickrios.github.io/static/media/mobile-header-bg.c0c2eafc.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9d28ddb4650fc8bb6d3d6f41b5bb5d80.js"
+  "/patrickrios.github.io/precache-manifest.4d5b02de23377ba7afe9b89fde87b5fc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/patrickrios.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
