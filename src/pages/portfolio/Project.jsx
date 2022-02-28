027c9ff9c3ab3c1b77bb092bc22dd2ced4a1fb7a
Patrick Rios <patrick.33.rios@hotmail.com>
2022-02-28 04:26:04 -0400

    rebuild portfolio project items

--- src/pages/portfolio/Project.jsx
+++ src/pages/portfolio/Project.jsx
@@ -1,56 +1,30 @@
 import React, { useContext } from 'react';
 import './styles/project.css'
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome' 
-import { faEye as preview } from '@fortawesome/free-regular-svg-icons'
-import {faGithub as github}  from '@fortawesome/free-brands-svg-icons'
 import { AppContext } from '../../contexts/AppContext';
 
 function Project(props) {
 
-    const { portfolio, lang } = useContext(AppContext)
+    const { lang } = useContext(AppContext)
 
-    function renderIcon( name ){
-        return(
-            <img src={`${process.env.PUBLIC_URL}/techs/${name}.png`} alt={name} />
-        )
-    }
+    const Overlay = () => <div className="project-about">
+        <h3>{props.datas.name}</h3>
+        <p className="project-description">
+            {props.datas.desc[lang]}
+        </p>
+        <div className="project-links">
+            { props.datas.links.map( 
+                link => <a href={link.url} className={`project-`+link.style}>{link.name}</a>
+            )}
+        </div>
+    </div> 
+
 
     return (
         <div className="portfolio-item">
-            <div className="project-about">
-                <header>
-                    <p>
-                        <img className="project-logo" src={props.datas.logo} alt={props.datas.name}/>                        
-                    </p>
-                    <div className="techs-list">
-                        {
-                            props.datas.techs.map( icon =>renderIcon(icon))
-                        }
-                        <span className="desk-only">{props.datas.level[lang]}</span>
-                    </div>
-                </header>
-                <p className="project-description">
-                    {props.datas.desc[lang]}
-                </p>
-                <div className="project-links">
-                    {
-                        (props.datas.live !== null) &&
-                        <a href={props.datas.live} className="project-live-demo">
-                            <FontAwesomeIcon icon={preview} /> 
-                            {portfolio.demo[lang]}
-                        </a>
-                    }
-                    
-                    <a href={props.datas.repo} className="project-source-code">
-                        <FontAwesomeIcon icon={github} /> 
-                        {portfolio.source[lang]}
-                    </a>
-                </div>
-            </div> 
-
-            <div className="project-snap desk-only">
+            <div className="project-snap">
                 <img src={props.datas.snap} alt="Project snap"/>
             </div>
+            <Overlay />
         </div>
     );
 }
