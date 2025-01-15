bb0c37ddaa7c7ee3f7be4ec373f7030862ab8584
patrickrios <patrick.33.rios@hotmail.com>
2025-01-14 23:48:34 -0300

    refact: add main menu to mobile navigation

--- src/components/top-menu/top-menu.tsx
+++ src/components/top-menu/top-menu.tsx
@@ -1,9 +1,15 @@
 import css from "./top-menu.module.css";
 import { LanguageToggle, ThemeToggle } from "../theme-toggle/ThemeToggle";
-import { topMenuItems, socialLinks } from "./menu-items-text";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AppContext } from "../../contexts/AppContext";
 import Link from "next/link";
+import { 
+  topMenuItems, 
+  socialLinks, 
+  MenuIcon, 
+  CloseMenuIcon, 
+  MenuSeparator 
+} from "./menu-items-text";
 
 const upper = (item: string) =>{
   return item.toUpperCase();
@@ -11,10 +17,22 @@ const upper = (item: string) =>{
 
 export const TopMenu = () => {
   const { lang } = useContext(AppContext);
+  const [shouldShowMenu, setShowMenu ] = useState<boolean>(false);
+
+  const showMenu = () => {
+    setShowMenu(prev_status => !prev_status)
+  };
+
   return(
     <nav className={css.topMenu}>
         <ul>
             <div className={css.socialButtons}>
+              <button 
+                className={css.mobileButton} 
+                onClick={showMenu}
+              >
+                <MenuIcon/>
+              </button>
               {socialLinks?.map( link => (
                 <Link 
                   href={link?.url}
@@ -27,16 +45,22 @@ export const TopMenu = () => {
                 </Link>
               ))}
             </div>
-            <div className={css.menuItems}>
-                {topMenuItems?.map(( item, index) => {
-                  return(
+            <div className={`${css.menuItems} ${shouldShowMenu && css.showMenu}`}>
+                <CloseMenuIcon 
+                  onClose={showMenu} 
+                  styleClass={css.closeMenuIcon}
+                />
+                { topMenuItems?.map((item, index) => (
                     <>
                       <li key={`top-menu-item~${index}`}>
                         { upper(item[lang]) }
                       </li>
-                      { index < topMenuItems?.length-1 && <MenuSeparator/>}
+                      { 
+                        index < topMenuItems?.length-1 && 
+                        <MenuSeparator styleClass={css.menuSeparator}/>
+                      }
                     </>
-                )})}
+                ))}
             </div>
             <div className={css.controls}>
                 <ThemeToggle/>
@@ -44,13 +68,6 @@ export const TopMenu = () => {
             </div>
         </ul>
     </nav>
-)};
-
-const MenuSeparator = () => {
-  return(
-    <svg width="6" height="6" viewBox="0 0 6 6" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M0 3L3 0L6 3L3 6L0 3Z" fill="#323232"/>
-    </svg>
 )};
 
 export default TopMenu;
