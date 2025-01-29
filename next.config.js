1ccbe19570b6a4895e3b68cf3180053a2fc31177
patrickrios <patrick.33.rios@hotmail.com>
2025-01-28 21:57:36 -0300

    chore: set config files to deploy nextjs to gh-pages

--- next.config.js
+++ next.config.js
@@ -1,19 +1,20 @@
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
+//const isGithubActions = process.env.GITHUB_ACTIONS || false;
+//
+//if (isGithubActions) {
+//  // Pega o nome do repositório do GitHub Actions
+//  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
+//  assetPrefix = `/${repo}`;
+//  basePath = `/${repo}`;
+//}
 
-let assetPrefix = '';
-let basePath = '';
-
-if (isGithubActions) {
-  // Pega o nome do repositório do GitHub Actions
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
-  assetPrefix = `/${repo}`;
-  basePath = `/${repo}`;
-}
+/** @type {import('next').NextConfig} */
 
 const nextConfig = {
   output: 'export',
-  assetPrefix,
-  basePath,
+  basePath: '', //patrickrios.github.io
+  assetPrefix: '',///patrickrios.github.io
+  trailingSlash: true,
+  distDir: 'dist',
   images: {
     unoptimized: true,
   },
