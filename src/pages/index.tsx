a89f682759a810860bd27c0cefd73b013b9f5dd0
patrickrios <patrick.33.rios@hotmail.com>
2025-03-20 04:29:49 -0300

    styles: remove unuseful import

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,6 @@ import { WhatsAppButton } from "../components/whatsapp-btn/Whatsapp";
 import Head from "next/head";
 import { ThemeContext } from "../contexts/ThemeContext";
 import { Footer } from "../components/footer/footer";
-import { ServicesSection } from "../components/services-section/services";
 
 export const Homepage = () =>{
     const { content } = useContext(ModalContext);
