021a8111997d254b71a25d409bea8bf2e6c73b39
Patrick <patrick.33.rios@hotmail.com>
2026-08-27 10:57:35 -0400

    refact: redesign header layout

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -5,75 +5,64 @@ import { AppContext } from "../../contexts/AppContext";
 import headerText from "./header-text.json";
 import parse from "html-react-parser";
 import Link from "next/link";
-import { socialItems } from "./social-items";
+import { socialItems, Godot, Aseprite } from "./social-items";
 
 export const Header = () => {
     const { lightMode } = useContext(ThemeContext);
     const { lang } = useContext(AppContext);
 
-    const PortugueseProfile = () =>{
-        return(
-        <>
-            <h2>{parse(headerText['headLine'][lang])}</h2>
-            <p className={`${css.taglinePtBr} ${lightMode && css.light}`}>
-                {parse(headerText['tagLine'][lang])}
-            </p>
-        </>
-    )}
-
-    const EnglishProfile = () => {
-        return (
-            <>
-                <p className={`${css.taglineEng} ${lightMode && css.light}`}>
-                    {parse(headerText['tagLine'][lang])}
-                </p>
-                <h2>{parse(headerText['headLine'][lang])}</h2>
-            </>
-    )}
-
     return(
         <header className={`${css.header} ${lightMode && css.light} center-container`}>
             <div className={css.profile}>
                 <picture>
-                    <source srcSet="/images/profile/profile-photo-180.svg" media="(min-width: 600px)" />
-                    <source srcSet="/images/profile/profile-photo-260.svg" media="(min-width: 1000px)" />
-                    <img src="/images/profile/profile-photo-140.svg" alt="MDN" />
+                    <source srcSet="/images/profile/patrickrios-pixel-180-min.png" media="(min-width: 600px)" />
+                    <source srcSet="/images/profile/patrickrios-pixel-260-min.png" media="(min-width: 1000px)" />
+                    <img src="/images/profile/patrickrios-pixel-140-min.png" alt="MDN" />
                 </picture>
                 <div className={css.profileHeadline}>
-                    { lang === 'eng' ? 
-                        <EnglishProfile/> : 
-                        <PortugueseProfile/>
-                    }
+                    <h2>Patrick Rios</h2>
+                    <p className={`${lightMode && css.light}`}>
+                        INDIE GAME DEV
+                    </p>
                 </div>
-                <div className={css.actionButtons}>
-                    <div className={`${css.getInTouchButton} ${ lightMode ? css.light : ''}`}>
-                        {socialItems?.map( link => (
-                            <Link 
-                                href={link?.url}
-                                target="_black" 
-                                title={link?.title}
-                                className={`${css[link?.styleClass]} ${lightMode ? css.lightA : ''}`}
-                                key={`social-link~${link?.styleClass}`}
-                            >
-                                { link?.icon }
-                            </Link>
-                        ))}
+                
+                <div className={css.description}>
+                    <div className={css.textDescription}>
+                        <p>{headerText.description[lang]}</p>
+                        <p className={css.stackDesc}>
+                            <Godot/> Godot + <Aseprite/> Aseprite + <img src="/images/icon/cafezito-Sheet-min.png"/> Café
+                        </p>
                     </div>
-                    <a 
-                        href={ 
-                            lang === "eng" ? 
-                                "/files/patrick_rios_developer_resume.pdf" : 
-                                "/files/patrick_rios_desenvolvedor_cv.pdf"
-                        } 
-                        className={css.downloadCvButton}
-                        download={ 
-                            lang === "eng" ? 
-                                "patrick-rios-web-developer-resume.pdf" : 
-                                "patrick-rios-desenvolvedor-web-cv.pdf"
-                        }
-                    >
-                        {lang === 'eng' ? 'Download resume' : 'Baixar CV'}
-                    </a>
+                    <div className={css.actionButtons}>
+                        <div className={`${css.getInTouchButton} ${ lightMode ? css.light : ''}`}>
+                            {socialItems?.map( link => (
+                                <Link 
+                                    href={link?.url}
+                                    target="_black" 
+                                    title={link?.title}
+                                    className={`${css[link?.styleClass]} ${lightMode ? css.lightA : ''}`}
+                                    key={`social-link~${link?.styleClass}`}
+                                >
+                                    { link?.icon }
+                                </Link>
+                            ))}
+                        </div>
+                        <a 
+                            href={ 
+                                lang === "eng" ? 
+                                    "/files/patrick_rios_developer_resume.pdf" : 
+                                    "/files/patrick_rios_desenvolvedor_cv.pdf"
+                            } 
+                            className={css.downloadCvButton}
+                            download={ 
+                                lang === "eng" ? 
+                                    "patrick-rios-web-developer-resume.pdf" : 
+                                    "patrick-rios-desenvolvedor-web-cv.pdf"
+                            }
+                        >
+                            {lang === 'eng' ? 'Download resume' : 'Baixar CV'}
+                        </a>
+                    </div>
                 </div>
             </div>
             <picture className={css.headerBackground}>
